38c916e257502f103df1791e537f4e4ac6550c2d
Rick <king.matthews.rickey@live.mercer.edu>
2024-03-25 15:31:47 -0400

    Added a Log in form and Customer Registration Form with

---
+++ FinalProject/LoginForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            //Open up the customer registration form for a new user
+            CustomerRegistrationForm registrationForm = new CustomerRegistrationForm();
+            registrationForm.ShowDialog();
+        }
+    }
+}
